9ee5a7d4a755b1519fae04048726feb994c6b490
Gradiyanto Putera Husein <gradiph@gmail.com>
2023-10-08 16:53:00 +0700

    cast to boolean

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,13 @@ class Answer extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected function isCorrect(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value == 1,
+        );
+    }
 
     public function results()
     {
